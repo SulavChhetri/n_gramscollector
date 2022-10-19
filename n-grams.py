860e88bc3ca55161c2d238efaebbf9300374fbfb
Xulav <sulavthapachhetri@gmail.com>
2022-10-19 10:56:33 +0545

    final

--- n-grams.py
+++ n-grams.py
@@ -36,8 +36,8 @@ def ngramcreator(strings,n_grams):
         return finallist
 
 def n_grams():
-    bidict = defaultdict(lambda:0)
-    tridict = defaultdict(lambda:0)
+    bidict = defaultdict(int)
+    tridict = defaultdict(int)
     data = pd.read_csv('./files/3_govt_urls_state_only.csv')
     data = data['Note']
     for item in data:
@@ -47,18 +47,14 @@ def n_grams():
             n_gram = ngramcreator(nostopword_note,number)
             for i in n_gram:
                 if number==2:
-                    if i not in bidict.keys():
-                        bidict[i]=1
-                    else:
-                       bidict[i] =bidict[i]+1
+                    bidict[i]+=1
+
                 else:
-                    if i not in tridict.keys():
-                        tridict[i]=1
-                    else:
-                       tridict[i] =tridict[i]+1
+                    tridict[i]+=1
 
     templist1 = sorted(bidict.items(),key=takeSecond,reverse=True)[:20]
     templist2 = sorted(tridict.items(),key=takeSecond,reverse=True)[:20]
+
     for item in templist1:
         n_grams_40.append(item[0])
     
